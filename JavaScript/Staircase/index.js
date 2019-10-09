c467e335203352277e9d369d805d119aa2ffe4bb
mark1ns0n <6otan1dze@gmail.com>
2019-10-09 22:29:02 +0300

    add Staircase

---
+++ JavaScript/Staircase/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the staircase function below.
+function staircase(n) {
+    const line = new Array(n + 1).join(' ');
+    for (let i = 0; i < n; i++) {
+        console.log(new Array(n - i).join(' ') + new Array(i + 2).join('#'));
+    }
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    staircase(n);
+}
+
