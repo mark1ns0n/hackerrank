d52e20f31550b474348ff4859565679be33b54ad
mark1ns0n <6otan1dze@gmail.com>
2019-10-20 18:19:33 +0300

    add Breaking the Records

---
+++ JavaScript/BreakingTheRecords/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the breakingRecords function below.
+function breakingRecords(scores) {
+    const result = [0, 0];
+    let min = scores[0];
+    let max = scores[0];
+
+    for (let i in scores) {
+        if (min > scores[i]) {
+            min = scores[i];
+            result[1]++;
+        }
+        if (max < scores[i]) {
+            max = scores[i];
+            result[0]++;
+        }
+    }
+
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const scores = readLine().split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
+
